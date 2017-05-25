c02b189648ad1c30c14c6cb3b3a87715f15962cd
Pandarati <akajonathanmckinley@gmail.com>
2017-05-25 19:05:37 -0400

    Implemented ServerInfo class that gets the start time of the server

---
+++ src/codeu/chat/common/ServerInfo.java
@@ -0,0 +1,18 @@
+package codeu.chat.common;
+
+public final class ServerInfo {
+
+    public final Time startTime;
+
+    public ServerInfo() {
+        this.startTime = Time.now();
+    }
+
+    public ServerInfo(Time startTime){
+        this.startTime = startTime;
+    }
+
+
+    
+
+}
